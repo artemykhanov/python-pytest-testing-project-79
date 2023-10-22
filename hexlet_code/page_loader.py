a84ec776dd061c5d537a9fbc145b5b2ba111ff34
Artemy Khanov <artemy.khanov@gmail.com>
2023-10-23 00:44:47 +0300

    implement page_loader

--- hexlet_code/page_loader.py
+++ hexlet_code/page_loader.py
@@ -1,4 +1,26 @@
+import string
+from pathlib import Path
+
+import requests
+
+
+def get_filename_for_url(url: str) -> str:
+    _, url_without_scheme = url.split('://', maxsplit=1)
+    filename = ''
+    for s in url_without_scheme:
+        if s not in string.ascii_letters + string.digits:
+            filename += '-'
+        else:
+            filename += s
+
+    return f'{filename}.html'
 
 
 def download(url: str, directory: str) -> str:
-    return ''
+    response = requests.get(url)
+    filename = get_filename_for_url(url)
+    filepath = Path(directory) / filename
+    # filepath.write_text(response.text)
+    with open(filepath, 'w') as f:
+        f.write(response.text)
+    return str(filepath)
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -2,7 +2,7 @@ import sys
 
 import pytest
 
-from hexlet_code.page_loader import download
+from hexlet_code.page_loader import download, get_filename_for_url
 
 
 @pytest.fixture
@@ -10,11 +10,30 @@ def expected_file_path():
     return 'file_fixtures/expected.html'
 
 
-def test_download(tmp_path, expected_file_path):
-    url = ''
-    directory = tmp_path / 'test_directory'
-    print(directory)
-    file_path = download(url, str(directory))
-    with open(file_path) as f, open(expected_file_path) as expected_file:
-        assert f.read() == expected_file.read()
+def get_file_fixture_content(filename: str) -> str:
+    with open(f'tests/file_fixtures/{filename}') as f:
+        return f.read()
 
+
+def test_download_simple_success(tmp_path, requests_mock):
+    url = 'https://example.com/simple'
+    expected_file_name = 'example-com-simple.html'
+    expected_content = get_file_fixture_content('expected.html')
+    requests_mock.get(url, text=expected_content)
+
+    file_path = download(url, str(tmp_path))
+
+    assert file_path == str(tmp_path / expected_file_name)
+    with open(file_path) as f:
+        assert f.read() == expected_content
+
+
+@pytest.mark.parametrize(
+    ('url', 'filename'),
+    (
+        ('https://example.com/simple.html', 'example-com-simple-html.html'),
+        ('https://sub-domain.example.com/a_b(1)#23-__--5', 'sub-domain-example-com-a-b-1--23-----5.html'),
+    )
+)
+def test_get_filename_for_url(url, filename):
+    assert get_filename_for_url(url) == filename
