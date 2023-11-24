db50f8197e124a8d625a2d60fdd0ff936dadd54f
Artemy Khanov <artemy.khanov@gmail.com>
2023-11-24 21:31:25 +0300

    download images from html pages

--- hexlet_code/page_loader.py
+++ hexlet_code/page_loader.py
@@ -1,26 +1,85 @@
+import os.path
 import string
 from pathlib import Path
+from urllib import parse
 
 import requests
+from bs4 import BeautifulSoup
+
+# todo: использовать дата классы
 
 
-def get_filename_for_url(url: str) -> str:
-    _, url_without_scheme = url.split('://', maxsplit=1)
-    filename = ''
-    for s in url_without_scheme:
+def make_path_by_url(url: str) -> str:
+    if "://" in url:
+        _, url = url.split('://', maxsplit=1)
+    path = ''
+    for s in url:
         if s not in string.ascii_letters + string.digits:
-            filename += '-'
+            path += '-'
         else:
-            filename += s
+            path += s
 
-    return f'{filename}.html'
+    return path
+
+
+def handle_files_in_html(html_text: str, files_directory: str, domain_with_scheme: str) -> tuple[list[dict], str]:
+    soup = BeautifulSoup(html_text, 'html.parser')
+    result = []
+    for img in soup.find_all('img'):
+        original_image_url_as_path = ensure_absolute_url(img['src'], domain_with_scheme)
+        without_ext, ext = original_image_url_as_path.rsplit('.', maxsplit=1)
+        # suffix = original_image_url_as_path.suffix
+        downloaded_image_path = str(Path(files_directory) / f'{make_path_by_url(without_ext)}.{ext}')
+
+        result.append({
+            'original_url': img['src'],
+            'downloaded_path': downloaded_image_path,
+        })
+
+        img['src'] = downloaded_image_path
+
+    return result, soup.prettify()
+
+
+def ensure_absolute_url(url: str, domain_with_scheme: str) -> str:
+    parsed_url = parse.urlparse(url)
+    if not parsed_url.netloc:
+        url = f'{domain_with_scheme}{url}'
+    return url
 
 
 def download(url: str, directory: str) -> str:
+    """
+
+    Получить содержимое страницы по указанному URL
+    Получить ссылки на ресурсы (картинки) из содержимого страницы и заменить их внутри страницы на путь к локальному файлу
+    Скачать ресурсы в указанную директорию
+    Сохранить страницу в указанную директорию
+    """
+    parsed_url = parse.urlparse(url)
+    domain_with_scheme = f'{parsed_url.scheme}://{parsed_url.netloc}'
     response = requests.get(url)
-    filename = get_filename_for_url(url)
+    page_text = response.text
+
+    path = make_path_by_url(url)
+    files_directory = f'{path}_files'
+
+    files, handled_page_text = handle_files_in_html(page_text, files_directory, domain_with_scheme)
+
+    if files and not os.path.exists(Path(directory) / files_directory):
+        os.mkdir(Path(directory) / files_directory)
+
+    for file in files:
+        file_url = ensure_absolute_url(file['original_url'], domain_with_scheme)
+        file_path = Path(directory) / file['downloaded_path']
+        response = requests.get(file_url)
+
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+
+    filename = f'{path}.html'
     filepath = Path(directory) / filename
-    # filepath.write_text(response.text)
+
     with open(filepath, 'w') as f:
-        f.write(response.text)
+        f.write(handled_page_text)
     return str(filepath)
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,16 +1,17 @@
-import sys
+import os.path
 
 import pytest
+import requests_mock
 
-from hexlet_code.page_loader import download, get_filename_for_url
-
-
-@pytest.fixture
-def expected_file_path():
-    return 'file_fixtures/expected.html'
+from hexlet_code.page_loader import download, make_path_by_url
 
 
-def get_file_fixture_content(filename: str) -> str:
+def get_file_fixture_bytes(filename: str) -> bytes:
+    with open(f'tests/file_fixtures/{filename}', 'rb') as f:
+        return f.read()
+
+
+def get_file_fixture_text(filename: str) -> str:
     with open(f'tests/file_fixtures/{filename}') as f:
         return f.read()
 
@@ -18,22 +19,48 @@ def get_file_fixture_content(filename: str) -> str:
 def test_download_simple_success(tmp_path, requests_mock):
     url = 'https://example.com/simple'
     expected_file_name = 'example-com-simple.html'
-    expected_content = get_file_fixture_content('expected.html')
-    requests_mock.get(url, text=expected_content)
+    expected_text = get_file_fixture_text('simple.html')
+    requests_mock.get(url, text=expected_text)
 
     file_path = download(url, str(tmp_path))
 
     assert file_path == str(tmp_path / expected_file_name)
     with open(file_path) as f:
-        assert f.read() == expected_content
+        assert f.read() == expected_text
 
 
 @pytest.mark.parametrize(
-    ('url', 'filename'),
+    ('url', 'path'),
     (
-        ('https://example.com/simple.html', 'example-com-simple-html.html'),
-        ('https://sub-domain.example.com/a_b(1)#23-__--5', 'sub-domain-example-com-a-b-1--23-----5.html'),
+        ('https://example.com/simple.html', 'example-com-simple-html'),
+        ('https://sub-domain.example.com/a_b(1)#23-__--5', 'sub-domain-example-com-a-b-1--23-----5'),
+        ('https://ru.hexlet.io/assets/professions/python', 'ru-hexlet-io-assets-professions-python'),
     )
 )
-def test_get_filename_for_url(url, filename):
-    assert get_filename_for_url(url) == filename
+def test_make_path_by_url(url, path):
+    assert make_path_by_url(url) == path
+
+
+def test_download_html_with_files(tmp_path):
+    url = 'https://ru.hexlet.io/courses'
+    file_url = 'https://ru.hexlet.io/assets/professions/python.png'
+    another_domain_file_url = 'https://cdn2.hexlet.io/assets/error-pages/404-83b6e8d08445469eb3b4347ac5cfb08d98f3d7ba9a8e4134bf7070b227a42a2a.svg'  # E501
+
+    with requests_mock.Mocker() as req_mock:
+        req_mock.get(url, text=get_file_fixture_text('html-with-files.html'))
+        req_mock.get(file_url, content=get_file_fixture_bytes('python.png'))
+        req_mock.get(another_domain_file_url, content=get_file_fixture_bytes('404-error.svg'))
+
+        file_path = download(url, str(tmp_path))
+
+    expected_downloaded_html = get_file_fixture_text('html-with-files-downloaded.html')
+
+    with open(file_path) as f:
+        content = f.read()
+        assert content == expected_downloaded_html
+
+    filepath = str(tmp_path / 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-python.png')
+    filepath_2 = str(tmp_path / 'ru-hexlet-io-courses_files/cdn2-hexlet-io-assets-error-pages-404-83b6e8d08445469eb3b4347ac5cfb08d98f3d7ba9a8e4134bf7070b227a42a2a.svg')
+
+    assert os.path.exists(filepath)
+    assert os.path.exists(filepath_2)
